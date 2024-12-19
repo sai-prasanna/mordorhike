61d0aecd3b07baa98998d7336c65313076214b49
sai-prasanna <sai.r.prasanna@gmail.com>
2024-12-19 18:24:29 +0100

    Fix the discretization logic

--- src/powm/envs/mordor.py
+++ src/powm/envs/mordor.py
@@ -329,13 +329,7 @@ class MordorHike(gym.Env):
         self.particles = self.particles[indices]
 
     def _get_belief(self):
-        return np.concatenate(
-            [
-                self.particles[..., :2],
-                (self.particles[..., 2:] - np.pi) / np.pi,
-            ],
-            axis=-1,
-        )
+        return self.particles
 
     def render(self):
         if self.render_mode is None:
@@ -571,18 +565,18 @@ class MordorHike(gym.Env):
         # Choose the most probable action for the first step
         return np.argmax(log_probs[0])
 
-    def discretize_belief(self, belief, grid_x_y_size=0.1):
+    def discretize_belief(self, belief, grid_x_y_size=0.1, grid_angle_size=np.pi/2):
         n_bins_x = np.ceil((self.map_upper_bound[0] - self.map_lower_bound[0]) / grid_x_y_size).astype(int)
         n_bins_y = np.ceil((self.map_upper_bound[1] - self.map_lower_bound[1]) / grid_x_y_size).astype(int)
-        
+        n_bins_angle = np.ceil(2*np.pi / grid_angle_size).astype(int)
         # Use numpy's histogram2d for better binning
-        grid, _, _ = np.histogram2d(
-            belief[:, 0],  # x coordinates
-            belief[:, 1],  # y coordinates
-            bins=[n_bins_x, n_bins_y],
+        grid, _ = np.histogramdd(
+            belief[:, :3],  # x, y, angle coordinates
+            bins=[n_bins_x, n_bins_y, n_bins_angle],
             range=[
                 [self.map_lower_bound[0], self.map_upper_bound[0]],
-                [self.map_lower_bound[1], self.map_upper_bound[1]]
+                [self.map_lower_bound[1], self.map_upper_bound[1]],
+                [0, 2*np.pi]
             ],
             density=True  # This automatically normalizes the histogram
         )
@@ -659,6 +653,7 @@ def main():
             print(f"Time taken: {end_time - start_time}")
             done = terminated or truncated
             print(f"Action: {action}, Observation: {obs}, Reward: {rew}, Done: {done}")
+            
 
             cum_rew += rew
 
